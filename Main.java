be2abcaf0210a085d12c165e0b782417a39d40bd
Harison Heim <harisonheim@gmail.com>
2025-03-28 23:05:07 -0300

    Exercicio 2
    Sistema para controle de estoque

--- Main.java
+++ Main.java
@@ -3,31 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matematica mat = new Matematica();
 
-        System.out.println("Escolha a operação: 1-Soma, 2-Subtração, 3-Divisão, 4-Multiplicação");
-        int opcao = sc.nextInt();
 
-        System.out.print("Digite o primeiro número: ");
-        double num1 = sc.nextDouble();
-        System.out.print("Digite o segundo número: ");
-        double num2 = sc.nextDouble();
+        Produto produto = new Produto(1, "Notebook", "15 polegadas", "Prata", 3000.00, 10);
+        produto.exibirProduto();
 
-        switch (opcao) {
-            case 1:
-                System.out.println("Resultado: " + mat.soma(num1, num2));
-                break;
-            case 2:
-                System.out.println("Resultado: " + mat.subtracao(num1, num2));
-                break;
-            case 3:
-                System.out.println("Resultado: " + mat.divisao(num1, num2));
-                break;
-            case 4:
-                System.out.println("Resultado: " + mat.multiplicacao(num1, num2));
-                break;
-            default:
-                System.out.println("Opção inválida!");
+        System.out.print("Quantas unidades deseja comprar? ");
+        int quantidade = sc.nextInt();
+
+        if (produto.venderProduto(quantidade)) {
+            Pagamento.realizarPagamento(produto, quantidade);
+            System.out.println("Estoque atual: " + produto.getQuantidadeEstoque());
+        } else {
+            System.out.println("Venda não realizada.");
         }
     }
 }
